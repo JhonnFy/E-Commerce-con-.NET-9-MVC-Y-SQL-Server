5e6f90ef98040949e9c548b8ac54ce9cca6f14b3
JhonFy <moesdeveloper1023@gmail.com>
2026-02-09 06:36:49 -0500

    Se Crean Las Clases: Category, Product, User

---
+++ ECommerce/ECommerce/Entities/Category.cs
@@ -0,0 +1,13 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace ECommerce.Entities
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+    }
+}
---
+++ ECommerce/ECommerce/Entities/Product.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace ECommerce.Entities
+{
+    public class Product
+    {
+        public int ProductId {get; set;}
+        public int CategoryId {get ; set;}
+        [Required]
+        public string Name {get; set;}
+        [Required]
+        public string Description { get; set;}
+        public decimal Price { get; set;}
+        public int Stock { get; set; }
+        public string? ImageName { get; set; } = null; /*Pueden Ingresar Vacios*/
+
+
+        //Fk Category
+        public Category? Category { get; set; }
+    }
+}
