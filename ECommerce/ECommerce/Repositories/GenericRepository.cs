b0bae4da9b22532a64b46ebf85c5e21175d56074
JhonFy <moesdeveloper1023@gmail.com>
2026-02-09 15:16:43 -0500

    Se Asigna El Repositorio

---
+++ ECommerce/ECommerce/Repositories/GenericRepository.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ECommerce.Context;
+
+namespace ECommerce.Repositories
+{
+    public class GenericRepository<TEntity>(AppDbContext _dbContext) where TEntity : class
+    {
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+                return await _dbContext.Set<TEntity>().ToListAsync();
+        }
+
+    }
+}
