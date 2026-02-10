8f257b1d297c4c33beb7a159b52926b44391a9fa
JhonFy <moesdeveloper1023@gmail.com>
2026-02-10 04:56:00 -0500

    Crear El Metodo GetAllAsync En GenericRepository

---
+++ ECommerce/ECommerce/Services/ProductServices.cs
@@ -0,0 +1,35 @@
+﻿using ECommerce.Entities;
+using ECommerce.Models;
+using ECommerce.Repositories;
+
+namespace ECommerce.Services
+{
+    public class ProductServices(
+    
+        /*Dependencias*/
+        GenericRepository<Category> _categoryRepository,
+        GenericRepository<Product> _productRepository,
+
+        /*Eniva Las Imagenes a wwwroot*/
+        IWebHostEnvironment _webHostEnvironment
+        )
+
+    {
+        /*Metodo Para Listar Productos*/
+        public async Task<IEnumerable<ProductVM>>GetAllAsync()
+        {
+            var products = await _productRepository.GetAllAsync();
+        }
+
+
+    }
+            
+
+
+
+
+
+
+
+    }
+}
