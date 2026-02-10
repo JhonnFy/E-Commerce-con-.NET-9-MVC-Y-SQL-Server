d022f36b7825b3f3907007d9a3defd5286c7285a
JhonFy <moesdeveloper1023@gmail.com>
2026-02-10 04:23:42 -0500

    Se Crea Model.Product, Con Base En Entities.Product

---
+++ ECommerce/ECommerce/Models/ProductVM.cs
@@ -0,0 +1,29 @@
+﻿using ECommerce.Entities;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace ECommerce.Models
+{
+    public class ProductVM
+    {
+        public int ProductId { get; set; }
+        public CategoryVM Category { get; set; }
+
+        public List<SelectListItem> Categories { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+       
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+        [Required]
+        public int Stock { get; set; }
+        public string? ImageName { get; set; } = null; /*Pueden Ingresar Vacios*/
+
+        /*Consume Las Img*/
+        public IFormFile? ImageFile { get; set; }
+
+    }
+}
